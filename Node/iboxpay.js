98f9c5e9b5bc1569d0d537d0af9e394c69796503
ip <ip@gmail.com>
2021-02-04 09:36:21 +0800

    up

--- Node/iboxpay.js
+++ Node/iboxpay.js
@@ -93,7 +93,7 @@ let middlerefreshTOKEN = [];
 if ($.isNode()) {
     // 没有设置 XP_CASH 则默认为 0 不提现
     CASH = process.env.XP_CASH || 15;
-    // 没有设置 XP_live 则默认0
+    // 没有设置 XP_live 则默认60
     LIVE = process.env.XP_live || 0;
 }
 if ($.isNode() && process.env.XP_iboxpayHEADER) {
