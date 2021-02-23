ad73258a40530d45932026d8c8fdb3d6ff5645ad
adw2s <hancw9508@gmail.com>
2021-02-23 17:19:16 +0800

    1
    1

---
+++ Task/flwCOOKIE.js
@@ -0,0 +1,58 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let flwurlVal= ``
+let flwheaderVal= ``
+let flwspbodyVal= ``
+let flwqwbodyVal= ``
+let flwydbodyVal= ``
+
+if (
+  process.env.FL_flwURL &&
+  process.env.FL_flwURL.indexOf('\n') > -1
+) {
+  flwurlVal = process.env.FL_flwURL.split('\n');
+} else {
+  flwurlVal = process.env.FL_flwURL.split();
+}
+if (
+  process.env.FL_flwHEADER &&
+  process.env.FL_flwHEADER.indexOf('\n') > -1
+) {
+  flwheaderVal = process.env.FL_flwHEADER.split('\n');
+} else {
+  flwheaderVal = process.env.FL_flwHEADER.split();
+}
+if (
+  process.env.FL_flwspBODY &&
+  process.env.FL_flwspBODY.indexOf('\n') > -1
+) {
+  flwspbodyVal = process.env.FL_flwspBODY.split('\n');
+} else {
+  flwspbodyVal = process.env.FL_flwspBODY.split();
+}
+if (
+  process.env.FL_flwqwBODY &&
+  process.env.FL_flwqwBODY.indexOf('\n') > -1
+) {
+  flwqwbodyVal = process.env.FL_flwqwBODY.split('\n');
+} else {
+  flwqwbodyVal = process.env.FL_flwqwBODY.split();
+}
+if (
+  process.env.FL_flwydBODY &&
+  process.env.FL_flwydBODY.indexOf('\n') > -1
+) {
+  flwydbodyVal = process.env.FL_flwydBODY.split('\n');
+}
+
+let flwcookie = {
+  flwurlVal: flwurlVal,
+  flwheaderVal: flwheaderVal,
+  flwspbodyVal: flwspbodyVal,
+  flwqwbodyVal: flwqwbodyVal, 
+  flwydbodyVal: flwydbodyVal,
+
+}
+
+module.exports =  flwcookie
