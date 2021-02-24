ade97f10b036361467307c67893cfe393be86bd8
adw2s <42504839+adw2s@users.noreply.github.com>
2021-02-24 15:02:13 +0800

    Update sqxs.js

--- Task/sqxs.js
+++ Task/sqxs.js
@@ -26,11 +26,11 @@
 hostname：ocean.shuqireader.com
 
 [rewrite_local]
-https://ocean.shuqireader.com/api/ad/v1/api/prize/lottery url script-request-body sqxsgetck.js
-https://ocean.shuqireader.com/api/activity/activity/v1/lottery/draw url script-request-body sqxsgetck.js
-https://ocean.shuqireader.com/api/activity/xapi/gold/record url script-request-body sqxsgetck.js
-https://ocean.shuqireader.com/api/prizecenter/xapi/prize/manual/receive url script-request-body sqxsgetck.js
-https://ocean.shuqireader.com/api/ad/v1/api/prize/readpage/pendant/lottery url script-request-body sqxsgetck.js
+https://ocean.shuqireader.com/api/ad/v1/api/prize/lottery url script-request-body https://raw.githubusercontent.com/adw2s/Action/main/Task/sqxsgetck.js
+https://ocean.shuqireader.com/api/activity/activity/v1/lottery/draw url script-request-body https://raw.githubusercontent.com/adw2s/Action/main/Task/sqxsgetck.js
+https://ocean.shuqireader.com/api/activity/xapi/gold/record url script-request-body https://raw.githubusercontent.com/adw2s/Action/main/Task/sqxsgetck.js
+https://ocean.shuqireader.com/api/prizecenter/xapi/prize/manual/receive url script-request-body https://raw.githubusercontent.com/adw2s/Action/main/Task/sqxsgetck.js
+https://ocean.shuqireader.com/api/ad/v1/api/prize/readpage/pendant/lottery url script-request-body https://raw.githubusercontent.com/adw2s/Action/main/Task/sqxsgetck.js
 
 [task_local]
 0 12 * * * sqxs.js, tag=书旗小说, enabled=true
